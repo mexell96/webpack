5eb5ced6404b624b9d959547a7d1f7ceb4fde30b
artem_github_desktop <mexell96@gmail.com>
2021-01-21 13:59:21 +0300

    Added sass loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./app.css";
+import "./style.scss";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <h1>Hello TMS! World!</h1>
+        <h3>and everybody</h3>
       </>
     );
   }
