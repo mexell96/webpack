117f43bdc6e2be2a957c35b8cce80252b59b468d
artem_github_desktop <mexell96@gmail.com>
2021-01-21 15:04:15 +0300

    Added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.(jpeg|png|jpg|svg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[hash:6].[ext]",
+          outputPath: "images",
+          publicPath: "images",
+          emitFile: true,
+          esModule: false,
+        },
+      },
     ],
   },
   plugins: [
